9ef9489522a7452128f3e1f1c70fd8ce57301816
Intybyte <vaan.testwork@gmail.com>
2024-10-04 17:32:00 +0200

    Cleanup a bit

--- src/main/java/at/pavlov/cannons/container/Target.java
+++ src/main/java/at/pavlov/cannons/container/Target.java
@@ -20,40 +20,32 @@ public record Target(
 
     public Target(Entity entity) {
         this(
-                entity.getName(),
-                switch (entity) {
-                    case Player player -> TargetType.PLAYER;
-                    case Monster monster -> TargetType.MONSTER;
-                    case Animals animals -> TargetType.ANIMAL;
-                    default -> TargetType.OTHER;
-                },
-                entity.getType(),
-                entity.getUniqueId(),
-                entity.getLocation().clone(),
-                entity instanceof LivingEntity livingEntity ?
-                        livingEntity.getEyeLocation().clone().add(0.0, -0.5, 0.0) :
-                        entity.getLocation().clone().add(0.0, 0.5, 0.0),
-                entity.getVelocity()
+            entity.getName(),
+            switch (entity) {
+                case Player player -> TargetType.PLAYER;
+                case Monster monster -> TargetType.MONSTER;
+                case Animals animals -> TargetType.ANIMAL;
+                default -> TargetType.OTHER;
+            },
+            entity.getType(),
+            entity.getUniqueId(),
+            entity.getLocation().clone(),
+            entity instanceof LivingEntity livingEntity ?
+                    livingEntity.getEyeLocation().clone().add(0.0, -0.5, 0.0) :
+                    entity.getLocation().clone().add(0.0, 0.5, 0.0),
+            entity.getVelocity()
         );
     }
 
     public Target(Cannon cannon) {
         this(
-                cannon.getCannonName(),
-                TargetType.CANNON,
-                null, // No EntityType for a cannon
-                cannon.getUID(),
-                cannon.getRandomBarrelBlock().clone().add(0.5, 0.0, 0.5),
-                cannon.getRandomBarrelBlock().clone().add(0.5, 0.5, 0.5),
-                cannon.getVelocity()
+            cannon.getCannonName(),
+            TargetType.CANNON,
+            null, // No EntityType for a cannon
+            cannon.getUID(),
+            cannon.getRandomBarrelBlock().clone().add(0.5, 0.0, 0.5),
+            cannon.getRandomBarrelBlock().clone().add(0.5, 0.5, 0.5),
+            cannon.getVelocity()
         );
-    }
-
-    @Override
-    public String toString() {
-        return "name: " + name +
-                " UID: " + uniqueId +
-                " location: " + centerLocation +
-                " velocity: " + velocity;
     }
 }
