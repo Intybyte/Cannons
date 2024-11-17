e0dfec0c0afaf5d726a65aae539fee02413f4479
Intybyte <vaan.testwork@gmail.com>
2024-11-17 16:18:47 +0100

    Add more methods to AsyncTaskManager

--- src/main/java/at/pavlov/cannons/dao/AsyncTaskManager.java
+++ src/main/java/at/pavlov/cannons/dao/AsyncTaskManager.java
@@ -3,11 +3,30 @@ package at.pavlov.cannons.dao;
 import at.pavlov.cannons.Cannons;
 import org.bukkit.Bukkit;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.function.Supplier;
 
 public class AsyncTaskManager {
     public static final ExecutorService executor = Executors.newFixedThreadPool(2);
     public static final Executor bukkit = Bukkit.getScheduler().getMainThreadExecutor(Cannons.getPlugin());
+
+    public static void fireSyncRunnable(Runnable runnable) {
+
+        if (Bukkit.isPrimaryThread()) {
+            runnable.run();
+        } else {
+            CompletableFuture.runAsync(runnable, bukkit).join();
+        }
+    }
+
+    public static <T> T fireSyncSupplier(Supplier<T> supplier) {
+        if (Bukkit.isPrimaryThread()) {
+            return supplier.get();
+        } else {
+            return CompletableFuture.supplyAsync(supplier, bukkit).join();
+        }
+    }
 }
