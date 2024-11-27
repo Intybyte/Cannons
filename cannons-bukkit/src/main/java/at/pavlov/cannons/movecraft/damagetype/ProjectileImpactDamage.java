973dc6778fa88c8374863a2a2ce8f3c93e29b7c3
Intybyte <vaan.testwork@gmail.com>
2024-11-27 21:02:42 +0100

    ProjectileImpactDamage class

---
+++ cannons-bukkit/src/main/java/at/pavlov/cannons/movecraft/damagetype/ProjectileImpactDamage.java
@@ -0,0 +1,10 @@
+package at.pavlov.cannons.movecraft.damagetype;
+
+import net.countercraft.movecraft.combat.features.tracking.types.Type;
+
+public class ProjectileImpactDamage implements Type {
+    @Override
+    public String toString() {
+        return "Cannons Impact";
+    }
+}
