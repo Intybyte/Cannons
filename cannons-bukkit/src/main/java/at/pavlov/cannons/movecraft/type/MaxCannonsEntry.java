14954928f1efc668814a18ee94c7c9b51574583b
Intybyte <vaan.testwork@gmail.com>
2024-11-27 18:53:25 +0100

    Add MaxCannons handling

---
+++ cannons-bukkit/src/main/java/at/pavlov/cannons/movecraft/type/MaxCannonsEntry.java
@@ -0,0 +1,46 @@
+package at.pavlov.cannons.movecraft.type;
+
+import lombok.Getter;
+import net.countercraft.movecraft.util.Pair;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Optional;
+
+public class MaxCannonsEntry {
+    @Getter
+    private final String name;
+    private final double max;
+    private final boolean numericMax;
+
+    public MaxCannonsEntry(String name, @NotNull Pair<Boolean, ? extends Number> max) {
+        this.name = name;
+        this.max = max.getRight().doubleValue();
+        this.numericMax = max.getLeft();
+    }
+
+    public boolean check(int count, int size) {
+        if (numericMax) {
+            return !(count > max);
+        }
+
+        double percent = 100D * count / size;
+        return !(percent > max);
+    }
+
+    /**
+     *
+     * @return Empty if no error, otherwise return the error
+     */
+    public Optional<String> detect(int count, int size) {
+        if (numericMax) {
+            if (count > max)
+                return Optional.of(String.format("%d > %d", count, (int) max));
+        } else {
+            double blockPercent = 100D * count / size;
+            if (blockPercent > max)
+                return Optional.of(String.format("%.2f%% > %.2f%%", blockPercent, max));
+        }
+
+        return Optional.empty();
+    }
+}
