8c2bdc1c5361d230ee13ef37dddb07509a2035fa
Intybyte <vaan.testwork@gmail.com>
2024-12-13 19:34:01 +0100

    Add documentation

--- api-internal/src/main/java/at/pavlov/internal/container/location/CannonVector.java
+++ api-internal/src/main/java/at/pavlov/internal/container/location/CannonVector.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Class used to specify a location without needing the specific world
+ */
 @AllArgsConstructor
 @NoArgsConstructor
 @Data public class CannonVector implements Cloneable {
