a46fea7c7ffce8f87f59caca48f0df22c6e1aa18
Intybyte <vaan.testwork@gmail.com>
2024-12-13 17:58:01 +0100

    Move some methods to SpawnEntityHolder

--- api-bukkit/src/main/java/at/pavlov/bukkit/container/BukkitEntityHolder.java
+++ api-bukkit/src/main/java/at/pavlov/bukkit/container/BukkitEntityHolder.java
@@ -3,164 +3,89 @@ package at.pavlov.bukkit.container;
 import at.pavlov.internal.CannonLogger;
 import at.pavlov.internal.container.SpawnEntityHolder;
 import at.pavlov.internal.enums.EntityDataType;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import org.bukkit.NamespacedKey;
+import org.bukkit.Registry;
 import org.bukkit.entity.EntityType;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.regex.MatchResult;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class BukkitEntityHolder extends SpawnEntityHolder<EntityType, PotionEffect> {
+
     public BukkitEntityHolder(String str) {
-        //split string at space
-        // NAME min-max
-        // ZOMBIE 1-2
-        // EntityData string
-
-        data = new HashMap<>();
-        potionEffects = new ArrayList<>();
-        Scanner s = new Scanner(str);
-        s.findInLine("(\\w+)\\s+(\\d+)-(\\d+)\\s*(.+)?");
-        MatchResult result = s.match();
-
-        try {
-            // 'AREA_EFFECT_CLOUD 1-1 {Particle:"entity_effect",Radius:5f,Duration:300,Color:16711680,Effects:[{Id:2b,Amplifier:3b,Duration:300,ShowParticles:0b},{Id:7b,Amplifier:1b,Duration:20,ShowParticles:0b},{Id:9b,Amplifier:2b,Duration:300,ShowParticles:0b},{Id:19b,Amplifier:2b,Duration:300,ShowParticles:0b}]}'
-
-            setType(EntityType.valueOf(result.group(1)));
-            setMinAmount(Integer.parseInt(result.group(2)));
-            if (getMinAmount() < 0) {
-                //Cannons.getPlugin().logSevere("Min amount for spawned entity: " + getType() + " is <0");
-                setMinAmount(0);
-            }
-            setMaxAmount(Integer.parseInt(result.group(3)));
-            if (getMaxAmount() > 100) {
-                //Cannons.getPlugin().logSevere("Max amount for spawned entity: " + getType() + " is >1000 and was reset to 10");
-                setMaxAmount(10);
-            }
-        } catch (Exception e) {
-            s.close();
-            CannonLogger.getLogger().log(Level.SEVERE, "Error: wrong entity type or min-max values");
-            return;
-        }
-
-        if (result.group(4) == null) {
-            s.close();
-            CannonLogger.getLogger().log(Level.WARNING, "type: " + getType() + " data: " + getData() + " min: " + minAmount + " max: " + maxAmount + " from str: " + str);
-            return;
-        }
-
-        // search for curly braces in as parameter
-        Pattern p = Pattern.compile("(?<=\\{)(.+)(?=\\})");
-        Matcher m = p.matcher(result.group(4));
-        String strdata = null;
-        if (m.find())
-            strdata = m.group(1);
-
-        //if there are no curly braces set the string to complete argument
-        if (strdata == null)
-            strdata = result.group(4).trim();
-
-        // convert entity data to map. Split the arguments by comma, but don't split inside parentheses []
-        // {Particle:"entity_effect",Radius:5f,Duration:300,Color:16711680,Effects:[{Id:2b,Amplifier:3b,Duration:300,ShowParticles:0b},{Id:7b,Amplifier:1b,Duration:20,ShowParticles:0b},{Id:9b,Amplifier:2b,Duration:300,ShowParticles:0b},{Id:19b,Amplifier:2b,Duration:300,ShowParticles:0b}]}
-        try {
-            for (String s1 : strdata.split("(?![^)(]*\\([^)(]*?\\)\\)),(?![^\\[]*\\])")) {
-                // separate in type and argument EFFECTS:1b
-                String[] s2 = s1.split(":(?![^\\[]*\\])");
-                // check if there are argument and value
-                if (s2.length <= 1) {
-                    CannonLogger.getLogger().log(Level.SEVERE, "" + s1 + " does not have an argument, use 'DURATION:10'");
-                    continue;
-                }
-
-                boolean found = false;
-                String com1 = s2[0].trim();
-                // if the type is an effect it can have multiple effects in parentheses
-                // effects:[{Id:2b,Amplifier:3b,Duration:300,ShowParticles:0b},{Id:7b,Amplifier:1b,Duration:20,ShowParticles:0b},{Id:9b,Amplifier:2b,Duration:300,ShowParticles:0b},{Id:19b,Amplifier:2b,Duration:300,ShowParticles:0b}]
-                if (!com1.equalsIgnoreCase("effects")) {
-                    for (EntityDataType dt : EntityDataType.values()) {
-                        // add new entries
-                        if (com1.equalsIgnoreCase(dt.getString())) {
-                            data.put(dt, s2[1].trim());
-                            found = true;
-                            break;
-                        }
-                    }
-                    if (!found) {
-                        CannonLogger.getLogger().log(Level.SEVERE, "'" + s2[0] + "' is not supported by Cannons");
-                    }
-                    continue;
-                }
-
-                String effects = s2[1].replaceAll("[\\[\\]]", "");
-
-                // isolate every effect inside the parentheses for every potion effect
-                // {Id:2b,Amplifier:3b,Duration:300,ShowParticles:0b},{Id:7b,Amplifier:1b,Duration:20,ShowParticles:0b},{Id:9b,Amplifier:2b,Duration:300,ShowParticles:0b},{Id:19b,Amplifier:2b,Duration:300,ShowParticles:0b}
-                for (String effect : effects.split(",(?![^\\{]*\\})")) {
-                    PotionEffectType type = null;
-                    int duration = 0;
-                    int amplifier = 0;
-                    boolean ambient = false;
-                    boolean particles = false;
-                    boolean icon = true;
-                    // remove the curly braces and split bei comma
-                    // {Id:2b,Amplifier:3b,Duration:300,ShowParticles:0b}
-                    for (String arg : effect.replaceAll("[\\{\\}]", "").split(",")) {
-                        // split between argument and value
-                        String[] s3 = arg.split(":");
-                        if (s3.length <= 1) {
-                            continue;
-                        }
-
-                        // check arguments type, duration, amplifier, ambient, particles, icon
-                        String val = s3[1].replaceAll("b", "");
-                        switch (s3[0].toLowerCase()) {
-                            case "id":
-                                type = PotionEffectType.getById(Integer.parseInt(val));
-                                break;
-                            case "duration":
-                                duration = Integer.parseInt(val);
-                                break;
-                            case "amplifier":
-                                amplifier = Integer.parseInt(val);
-                                break;
-                            case "ambient":
-                                ambient = Boolean.parseBoolean(val);
-                                break;
-                            case "showparticles":
-                                particles = Boolean.parseBoolean(val);
-                                break;
-                            case "icon":
-                                icon = Boolean.parseBoolean(val);
-                                break;
-                            default:
-                                CannonLogger.getLogger().log(Level.SEVERE, "'" + s3[0] + "' is not a correct potion effect argument. See Bukkit PotionType");
-                        }
-                    }
-                    //Cannons.getPlugin().logDebug("AREA OF EFFECT CLOUD potion type: " + type + " duration " + duration + " amplifier " + amplifier + " ambient " + ambient + " particles " + particles + " icon " + icon);
-                    if (type != null && duration > 0 && amplifier > 0)
-                        potionEffects.add(new PotionEffect(type, duration, amplifier, ambient, particles, icon));
-                }
-            }
-            s.close();
-            CannonLogger.getLogger().info("type: " + getType() + " data: " + getData() + " min: " + minAmount + " max: " + maxAmount + " from str: " + str);
-        } catch (Exception e) {
-            CannonLogger.getLogger().log(Level.SEVERE, "Error while converting " + str + ". Check formating (Zombie 1-2 EntityData)" + e);
-            setType(null);
-            setMinAmount(0);
-            setMaxAmount(0);
-        }
+        super(str);
     }
 
     public BukkitEntityHolder(EntityType type, int minAmount, int maxAmount, Map<EntityDataType, String> data) {
-        this.type = type;
-        this.minAmount = minAmount;
-        this.maxAmount = maxAmount;
-        this.data = data;
+        super(type, minAmount, maxAmount, data);
+    }
+
+    @Override
+    public void parseEntityType(String entityTypeStr) {
+        try {
+            setType(EntityType.valueOf(entityTypeStr));
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Invalid entity type: " + entityTypeStr, e);
+        }
+    }
+
+    @Override
+    public void createPotionEffect(JsonObject attributes) {
+        try {
+            PotionEffectType type = getPotionType(attributes.get("Id"));
+            int duration = getNumber(attributes.get("Duration"));
+            int amplifier = getNumber(attributes.get("Amplifier"));
+            boolean ambient = getBoolean(attributes, "Ambient", false);
+            boolean particles = getBoolean(attributes, "ShowParticles", false);
+            boolean icon = getBoolean(attributes, "Icon", true);
+
+            if (type != null && duration > 0 && amplifier > 0) {
+                potionEffects.add(new PotionEffect(type, duration, amplifier, ambient, particles, icon));
+            }
+        } catch (Exception e) {
+            logError("Invalid potion effect attributes", e, attributes.toString());
+        }
+    }
+
+    // TODO make a json utils
+    public static PotionEffectType getPotionType(JsonElement element) {
+        try {
+            int id = getNumber(element);
+            return PotionEffectType.getById(id);
+        } catch (NumberFormatException e) {
+            String str = element.getAsString();
+            if (str == null) {
+                CannonLogger.getLogger().severe("Effect type string cannot be null");
+                return null;
+            }
+
+            NamespacedKey potionKey = NamespacedKey.fromString(str);
+            if (potionKey == null) {
+                CannonLogger.getLogger().severe("Effect type key not found, format should be something like: minecraft:strength");
+                return null;
+            }
+
+            return Registry.EFFECT.get(potionKey);
+        }
+    }
+
+    public static int getNumber(JsonElement element) throws NumberFormatException {
+        var str = element.getAsString();
+        if (str.endsWith("b")) {
+            return Integer.parseInt(str.substring(0, str.length() - 1));
+        }
+
+        return Integer.parseInt(str);
+    }
+
+    public static boolean getBoolean(JsonObject obj, String key, boolean default_value) {
+        if (!obj.has(key)) {
+            return default_value;
+        }
+
+        return getNumber(obj.get(key)) == 1;
     }
 }
--- api-internal/src/main/java/at/pavlov/internal/container/SpawnEntityHolder.java
+++ api-internal/src/main/java/at/pavlov/internal/container/SpawnEntityHolder.java
@@ -1,10 +1,17 @@
 package at.pavlov.internal.container;
 
+import at.pavlov.internal.CannonLogger;
 import at.pavlov.internal.enums.EntityDataType;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
 
 @Data abstract public class SpawnEntityHolder<Entity, Potion> {
     protected Entity type;
@@ -12,4 +19,109 @@ import java.util.Map;
     protected int maxAmount;
     protected Map<EntityDataType, String> data;
     protected List<Potion> potionEffects;
+
+    public SpawnEntityHolder(Entity type, int minAmount, int maxAmount, Map<EntityDataType, String> data) {
+        this.type = type;
+        this.minAmount = minAmount;
+        this.maxAmount = maxAmount;
+        this.data = data;
+    }
+
+    public SpawnEntityHolder(String str) {
+        data = new HashMap<>();
+        potionEffects = new ArrayList<>();
+
+        try {
+            // Split the input string into parts
+            String[] parts = str.split(" ", 3);
+            if (parts.length < 2) {
+                throw new IllegalArgumentException("Input string must contain at least entity type and min-max values.");
+            }
+
+            // Parse entity type and min-max values
+            parseEntityType(parts[0]);
+            parseMinMax(parts[1]);
+
+            // Parse additional entity data if present
+            if (parts.length > 2) {
+                parseEntityData(parts[2]);
+            } else {
+                logWarning("No additional data provided: ", str);
+            }
+
+        } catch (Exception e) {
+            logError("Error parsing input", e, str);
+            resetEntityData();
+        }
+    }
+
+    public void resetEntityData() {
+        setType(null);
+        setMinAmount(0);
+        setMaxAmount(0);
+    }
+
+    public void parseEntityData(String dataStr) {
+        try {
+            // Parse the data string as JSON
+            JsonObject jsonObject = JsonParser.parseString(dataStr).getAsJsonObject();
+
+            for (Map.Entry<String, JsonElement> entry : jsonObject.entrySet()) {
+                String key = entry.getKey();
+                JsonElement value = entry.getValue();
+
+                if (key.equalsIgnoreCase("effects")) {
+                    parsePotionEffects(value);
+                } else {
+                    addEntityData(key, value.getAsString());
+                }
+            }
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Invalid entity data format: " + dataStr, e);
+        }
+    }
+
+    public void parseMinMax(String minMaxStr) {
+        String[] range = minMaxStr.split("-");
+        if (range.length != 2) {
+            throw new IllegalArgumentException("Invalid min-max format: " + minMaxStr);
+        }
+        setMinAmount(Math.max(0, Integer.parseInt(range[0])));
+        setMaxAmount(Math.min(100, Integer.parseInt(range[1])));
+    }
+
+    public void addEntityData(String key, String value) {
+        EntityDataType dataType = EntityDataType.lookup(key);
+        if (dataType == null) {
+            throw new IllegalArgumentException("Unsupported entity data key: " + key);
+        }
+
+        data.put(dataType, value);
+    }
+
+    public void parsePotionEffects(JsonElement effectsElement) {
+        if (!effectsElement.isJsonArray()) {
+            throw new IllegalArgumentException("Effects must be a JSON array.");
+        }
+
+        effectsElement.getAsJsonArray().forEach(effectElement -> {
+            if (!effectElement.isJsonObject()) {
+                throw new IllegalArgumentException("Each effect must be a JSON object.");
+            }
+
+            JsonObject effectObject = effectElement.getAsJsonObject();
+            createPotionEffect(effectObject);
+        });
+    }
+
+    public abstract void createPotionEffect(JsonObject attributes);
+    public abstract void parseEntityType(String entityTypeStr);
+
+    protected void logError(String message, Exception e, String context) {
+        CannonLogger.getLogger().log(Level.SEVERE, message + (context != null ? ": " + context : ""), e);
+    }
+
+    protected void logWarning(String message, String context) {
+        CannonLogger.getLogger().log(Level.WARNING, message + (context != null ? ": " + context : ""));
+    }
 }
