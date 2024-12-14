68067d131f9742e889ee11dbe494e548f4731196
Intybyte <vaan.testwork@gmail.com>
2024-12-14 18:46:20 +0100

    Fix Projectile

--- api-bukkit/src/main/java/at/pavlov/bukkit/projectile/BukkitProjectile.java
+++ api-bukkit/src/main/java/at/pavlov/bukkit/projectile/BukkitProjectile.java
@@ -20,7 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BukkitProjectile extends Projectile<
-        Entity,
+        EntityType,
         PotionEffectType,
         BlockData,
         ParticleBuilder,
