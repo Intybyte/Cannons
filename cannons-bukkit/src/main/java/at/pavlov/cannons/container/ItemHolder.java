86b7e11d6d922717e9fa8c06750b0750fefce16f
Intybyte <vaan.testwork@gmail.com>
2025-01-12 10:56:34 +0100

    Refactor ItemHolder

--- cannons-bukkit/src/main/java/at/pavlov/cannons/container/ItemHolder.java
+++ cannons-bukkit/src/main/java/at/pavlov/cannons/container/ItemHolder.java
@@ -10,118 +10,86 @@ import org.bukkit.block.data.BlockData;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
 import java.util.logging.Level;
 
 
 //small class as at.pavlov.cannons.container for item id and data
-public class ItemHolder
-{
-	private Material material;
-	private String displayName;
-	private List<String> lore;
-	private boolean useTypeName;
+public class ItemHolder {
+    private Material material;
+    private String displayName;
+    private List<String> lore;
 
-	//half of this stuff is useless, keeping it for compatibity maybe for now
-	private static Class localeClass = null;
-	private static Class craftItemStackClass = null, nmsItemStackClass = null, nmsItemClass = null;
-	private static String OBC_PREFIX = Bukkit.getServer().getClass().getPackage().getName();
-	private static String NMS_PREFIX = OBC_PREFIX.replace("org.bukkit.craftbukkit", "net.minecraft.server");
-
-	public ItemHolder(ItemStack item)
-	{
-		useTypeName = false;
-        if (item == null){
-            material=Material.AIR;
-            displayName="";
+    public ItemHolder(ItemStack item) {
+        if (item == null) {
+            material = Material.AIR;
+            displayName = "";
             lore = new ArrayList<String>();
             return;
         }
 
-		material = item.getType();
+        material = item.getType();
 
         if (!item.hasItemMeta()) {
             return;
         }
 
         ItemMeta meta = item.getItemMeta();
-        if (meta.hasDisplayName() && meta.getDisplayName()!=null) {
-			displayName = meta.getDisplayName();
-		} else if (!meta.hasDisplayName()){
-            useTypeName = true;
+        if (meta.hasDisplayName() && meta.getDisplayName() != null) {
+            displayName = meta.getDisplayName();
+        } else if (!meta.hasDisplayName()) {
             displayName = getFriendlyName(item, true);
             //Cannons.getPlugin().logDebug("display name: " + displayName);
         } else {
-			displayName = "";
-		}
+            displayName = "";
+        }
 
-		boolean loreExists = meta.hasLore() && meta.getLore() != null;
+        boolean loreExists = meta.hasLore() && meta.getLore() != null;
         lore = loreExists ? meta.getLore() : new ArrayList<>();
     }
 
-    @Deprecated
-    public ItemHolder(int id)
-    {
-    	//not working
-        this(Material.AIR);
-    }
-
-    public ItemHolder(Material material)
-    {
+    public ItemHolder(Material material) {
         this(material, null, null);
     }
 
-	public ItemHolder(Material material, String description, List<String> lore)
-	{
+    public ItemHolder(Material material, String description, List<String> lore) {
         this.material = Objects.requireNonNullElse(material, Material.AIR);
 
-		Objects.requireNonNullElse(description, "");
+		description = Objects.requireNonNullElse(description, "");
         this.displayName = description == null ? "" : ChatColor.translateAlternateColorCodes('&', description);
         this.lore = Objects.requireNonNullElseGet(lore, ArrayList::new);
-	}
+    }
 
-	public ItemHolder(String str)
-	{
+    public ItemHolder(String str) {
         // data structure:
         // id;DESCRIPTION;LORE1;LORE2
         // HOE;COOL Item;Looks so cool;Fancy
-        try {
-			material = Material.AIR;
-			Scanner s = new Scanner(str).useDelimiter("\\s*;\\s*");
-
-			if (s.hasNext()) {
-				String next = s.next();
-				if (next != null)
-					this.material = Material.matchMaterial(next);
-				if (this.material == null) {
-					this.material = Material.AIR;
+        String[] entries = str.split(";");
+		lore = new ArrayList<>(entries.length - 2);
+		for (int i = 0; i < entries.length; i++) {
+			switch (i) {
+				case 0 -> {
+					material = Material.matchMaterial(entries[i]);
+					if (material == null) {
+						material = Material.AIR;
+					}
 				}
+
+				case 1 -> displayName = entries[i].replace('&', '§');
+				default -> lore.add(entries[i]);
 			}
+		}
+    }
 
-			displayName = s.hasNext() ? ChatColor.translateAlternateColorCodes('&', s.next()) : "";
-			lore = new ArrayList<>();
-
-			while (s.hasNext()) {
-				String nextStr = s.next();
-				if (!nextStr.isEmpty())
-					lore.add(nextStr);
-			}
-
-			s.close();
-		} catch(Exception e) {
-			Cannons.logger().log(Level.SEVERE,"[CANNONS] Error while converting " + str + ". Check formatting (minecraft:clock)");
-        }
-	}
-	
-	public SimpleBlock toSimpleBlock()
-	{
-		return new SimpleBlock(0, 0, 0, material);
-	}
-	
-	public ItemStack toItemStack(int amount)
-	{
-		material = material == null ? Material.AIR : material;
-		ItemStack item = new ItemStack(material, amount);
+    public ItemStack toItemStack(int amount) {
+        material = material == null ? Material.AIR : material;
+        ItemStack item = new ItemStack(material, amount);
         ItemMeta meta = item.getItemMeta();
         if (this.hasDisplayName())
             meta.setDisplayName(this.displayName);
@@ -129,64 +97,65 @@ public class ItemHolder
             meta.setLore(this.lore);
         item.setItemMeta(meta);
         return item;
-	}
+    }
 
-	/**
-	 * Creates a new BlockData instance for this Material, with all properties initialized to unspecified defaults.
-	 * @return BlockData instance
-	 */
-	public BlockData toBlockData()
-	{
-		return this.material.createBlockData();
-	}
+    /**
+     * Creates a new BlockData instance for this Material, with all properties initialized to unspecified defaults.
+     *
+     * @return BlockData instance
+     */
+    public BlockData toBlockData() {
+        return this.material.createBlockData();
+    }
 
-	/**
-	 * Creates a new BlockData instance for this Material, with all properties initialized to unspecified defaults, except for those provided in data.
-	 * @return BlockData instance
-	 */
-	public BlockData toBlockData(String string)
-	{
-		return this.material.createBlockData(string);
-	}
+    /**
+     * Creates a new BlockData instance for this Material, with all properties initialized to unspecified defaults, except for those provided in data.
+     *
+     * @return BlockData instance
+     */
+    public BlockData toBlockData(String string) {
+        return this.material.createBlockData(string);
+    }
 
     /**
      * compares the id of two Materials
+     *
      * @param material material to compare
      * @return true if both material are equal
      */
-	public boolean equals(Material material)
-	{
+    public boolean equals(Material material) {
         return material != null && material.equals(this.material);
     }
-	
-	/**
-	 * compares id and data, but skips data comparison if one is -1
-	 * @param item item to compare
-	 * @return true if both items are equal in data and id or only the id if one data = -1
-	 */
-	public boolean equalsFuzzy(ItemStack item)
-	{
-		ItemHolder itemHolder = new ItemHolder(item);
-		return equalsFuzzy(itemHolder);
-	}
-	
-	
-	/**
-	 * compares id and data, but skips data comparison if one is -1
-	 * @param item the item to compare
-	 * @return true if both items are equal in data and id or only the id if one data = -1
-	 */	
-	public boolean equalsFuzzy(ItemHolder item) {
+
+    /**
+     * compares id and data, but skips data comparison if one is -1
+     *
+     * @param item item to compare
+     * @return true if both items are equal in data and id or only the id if one data = -1
+     */
+    public boolean equalsFuzzy(ItemStack item) {
+        ItemHolder itemHolder = new ItemHolder(item);
+        return equalsFuzzy(itemHolder);
+    }
+
+
+    /**
+     * compares id and data, but skips data comparison if one is -1
+     *
+     * @param item the item to compare
+     * @return true if both items are equal in data and id or only the id if one data = -1
+     */
+    public boolean equalsFuzzy(ItemHolder item) {
         if (item == null) {
             return false;
         }
 
-		//System.out.println("item: " + item.getDisplayName() + " cannons " + this.getDisplayName());
+        //System.out.println("item: " + item.getDisplayName() + " cannons " + this.getDisplayName());
         //Item does not have the required display name
         if ((this.hasDisplayName() && !item.hasDisplayName()) || (!this.hasDisplayName() && item.hasDisplayName()))
             return false;
 
-		//Display name do not match
+        //Display name do not match
         if (item.hasDisplayName() && this.hasDisplayName() && !item.getDisplayName().equals(displayName))
             return false;
 
@@ -202,57 +171,56 @@ public class ItemHolder
         int size = similar.size();
         similar.retainAll(item.getLore());
 
-		if (similar.size() < size)
+        if (similar.size() < size)
             return false;
 
         return item.getType().equals(this.material);
     }
-	
-	/**
-	 * compares id and data, but skips data comparison if one is -1
-	 * @param block item to compare
-	 * @return true if both items are equal in data and id or only the id if one data = -1
-	 */
-	public boolean equalsFuzzy(Block block) {
-		//System.out.println("id:" + item.getId() + "-" + id + " data:" + item.getData() + "-" + data);
+
+    /**
+     * compares id and data, but skips data comparison if one is -1
+     *
+     * @param block item to compare
+     * @return true if both items are equal in data and id or only the id if one data = -1
+     */
+    public boolean equalsFuzzy(Block block) {
+        //System.out.println("id:" + item.getId() + "-" + id + " data:" + item.getData() + "-" + data);
         if (block == null) {
             return false;
         }
 
         return block.getType().equals(this.material);
     }
-	
-	public String toString()
-	{
-		return this.material + ":" + this.displayName + ":" + StringUtils.join(this.lore, ":");
-	}
 
-	public Material getType()
-	{
-		return this.material;
-	}
-	public void setType(Material material)
-	{
-		this.material = material;
-	}
-
-	public String getDisplayName() {
-		return displayName;
-	}
-
-    public boolean hasDisplayName(){
-        return this.displayName!= null && !this.displayName.equals("");
+    public String toString() {
+        return this.material + ":" + this.displayName + ":" + StringUtils.join(this.lore, ":");
     }
 
-	public List<String> getLore() {
-		return lore;
-	}
-
-    public boolean hasLore(){
-        return this.lore!=null && this.lore.size()>0;
+    public Material getType() {
+        return this.material;
     }
 
-	private static String capitalizeFully(String name) {
+    public void setType(Material material) {
+        this.material = material;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public boolean hasDisplayName() {
+        return this.displayName != null && !this.displayName.equals("");
+    }
+
+    public List<String> getLore() {
+        return lore;
+    }
+
+    public boolean hasLore() {
+        return this.lore != null && this.lore.size() > 0;
+    }
+
+    private static String capitalizeFully(String name) {
         if (name == null) {
             return "";
         }
@@ -269,20 +237,20 @@ public class ItemHolder
         for (String subName : name.split("_"))
             sbName.append(subName.substring(0, 1).toUpperCase() + subName.substring(1).toLowerCase()).append(" ");
 
-		return sbName.toString().substring(0, sbName.length() - 1);
+        return sbName.substring(0, sbName.length() - 1);
     }
 
-	private static String getFriendlyName(Material material) {
-		return material == null ? "Air" : getFriendlyName(new ItemStack(material), false);
-	}
+    private static String getFriendlyName(Material material) {
+        return material == null ? "Air" : getFriendlyName(new ItemStack(material), false);
+    }
 
-	private static String getFriendlyName(ItemStack itemStack, boolean checkDisplayName) {
-		if (itemStack == null || itemStack.getType() == Material.AIR) return "Air";
+    private static String getFriendlyName(ItemStack itemStack, boolean checkDisplayName) {
+        if (itemStack == null || itemStack.getType() == Material.AIR) return "Air";
 
-		if (checkDisplayName && itemStack.hasItemMeta() && itemStack.getItemMeta().hasDisplayName()) {
-			return itemStack.getItemMeta().getDisplayName();
-		}
+        if (checkDisplayName && itemStack.hasItemMeta() && itemStack.getItemMeta().hasDisplayName()) {
+            return itemStack.getItemMeta().getDisplayName();
+        }
 
-		return capitalizeFully(itemStack.getType().name().replace("_", " ").toLowerCase());
-	}
+        return capitalizeFully(itemStack.getType().name().replace("_", " ").toLowerCase());
+    }
 }
