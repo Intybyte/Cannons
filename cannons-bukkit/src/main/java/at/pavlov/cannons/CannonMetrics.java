bfac16909823d738a7cc5a5cd7b11cb57b36ccf7
Intybyte <vaan.testwork@gmail.com>
2025-02-08 23:09:53 +0100

    Add data to send

--- cannons-bukkit/src/main/java/at/pavlov/cannons/CannonMetrics.java
+++ cannons-bukkit/src/main/java/at/pavlov/cannons/CannonMetrics.java
@@ -1,13 +1,25 @@
 package at.pavlov.cannons;
 
+import at.pavlov.cannons.cannon.CannonDesign;
+import at.pavlov.cannons.cannon.CannonManager;
+import at.pavlov.cannons.cannon.DesignStorage;
+import at.pavlov.cannons.projectile.Projectile;
+import at.pavlov.cannons.projectile.ProjectileProperties;
+import at.pavlov.cannons.projectile.ProjectileStorage;
+import at.pavlov.cannons.utils.ParseUtils;
 import at.pavlov.internal.Hook;
 import at.pavlov.internal.HookManager;
 import org.bstats.bukkit.Metrics;
 import org.bstats.charts.AdvancedPie;
+import org.bstats.charts.SimplePie;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.entity.EntityType;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class CannonMetrics extends Metrics {
     public CannonMetrics(JavaPlugin plugin) {
@@ -19,8 +31,8 @@ public class CannonMetrics extends Metrics {
 
         this.addCustomChart(
                 new AdvancedPie("hooks", () -> {
-
                     Map<String, Integer> result = new HashMap<>();
+
                     if (!hookManager.isActive()) {
                         result.put("None", 1);
                         return result;
@@ -34,5 +46,81 @@ public class CannonMetrics extends Metrics {
                     return result;
                 })
         );
+
+        this.addCustomChart(
+                new SimplePie("cannons_amount", () -> getRange(CannonManager.getCannonList().size()))
+        );
+
+        this.addCustomChart(
+                new SimplePie("design_amount", () -> getRange(DesignStorage.getInstance().getCannonDesignList().size()))
+        );
+
+        this.addCustomChart(
+                new SimplePie("projectile_amount", () -> getRange(ProjectileStorage.getProjectileList().size()))
+        );
+
+        this.addCustomChart(
+                new AdvancedPie("gunpowder_materials", () -> {
+                    Map<String, Integer> result = new HashMap<>();
+
+                    for (CannonDesign design : DesignStorage.getInstance().getCannonDesignList()) {
+                        if (!design.isGunpowderNeeded()) {
+                            result.compute("None", this::adderCompute);
+                        } else {
+                            Material gunpowder = design.getGunpowderType().getType();
+                            String name = ParseUtils.normalizeName(gunpowder.name());
+                            result.compute(name, this::adderCompute);
+                        }
+                    }
+
+                    return result;
+                })
+        );
+
+        this.addCustomChart(
+                new AdvancedPie("projectile_entities", () -> {
+                    Map<String, Integer> result = new HashMap<>();
+
+                    ProjectileStorage.getProjectileList().stream()
+                            .map(Projectile::getProjectileEntity)
+                            .filter(Objects::nonNull)
+                            .map(EntityType::getKey)
+                            .map(NamespacedKey::getKey)
+                            .map(ParseUtils::normalizeName)
+                            .forEach(entityString -> result.compute(entityString, this::adderCompute));
+
+                    return result;
+                })
+        );
+
+        this.addCustomChart(
+                new AdvancedPie("projectile_properties", () -> {
+                    Map<String, Integer> result = new HashMap<>();
+
+                    for (Projectile projectile : ProjectileStorage.getProjectileList()) {
+                        var list = projectile.getPropertyList();
+                        if (list.isEmpty()) {
+                            result.compute("None", this::adderCompute);
+                            continue;
+                        }
+
+                        list.stream()
+                                .map(ProjectileProperties::getName)
+                                .map(ParseUtils::normalizeName)
+                                .forEach((name) -> result.compute(name, this::adderCompute));
+                    }
+
+                    return result;
+                })
+        );
+    }
+
+    private Integer adderCompute(String key, Integer value) {
+        return value == null ? 1 : value + 1;
+    }
+
+    private static String getRange(int amount) {
+        int range = amount - (amount % 10);
+        return String.valueOf(range);
     }
 }
