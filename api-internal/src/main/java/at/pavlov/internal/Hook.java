13e0cb8d5c982d2088e3c15ade71603302f7be78
Vaan1310 <61906290+Intybyte@users.noreply.github.com>
2025-02-10 12:29:16 +0100

    Fix/update-notifier (#18)
    * Save downloadUrl to print
    
    * Fix awkward nullability
    
    * Lazy log printing

--- api-internal/src/main/java/at/pavlov/internal/Hook.java
+++ api-internal/src/main/java/at/pavlov/internal/Hook.java
@@ -1,5 +1,7 @@
 package at.pavlov.internal;
 
+import org.jetbrains.annotations.NotNull;
+
 public interface Hook<H> {
     H hook();
     default boolean active() {
@@ -10,7 +12,7 @@ public interface Hook<H> {
     void onDisable();
     Class<? extends Hook<?>> getTypeClass();
 
-    default String enabledMessage() {
+    default @NotNull String enabledMessage() {
         return getTypeClass().getSimpleName() + " Enabled.";
     }
 }
--- api-internal/src/main/java/at/pavlov/internal/HookManager.java
+++ api-internal/src/main/java/at/pavlov/internal/HookManager.java
@@ -1,5 +1,7 @@
 package at.pavlov.internal;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -10,11 +12,11 @@ public class HookManager {
     /**
      * @return A defensive copy of the hook map
      */
-    public Map<Class<? extends Hook<?>>, Hook<?>> hookMap() {
+    public @NotNull Map<Class<? extends Hook<?>>, Hook<?>> hookMap() {
         return Collections.unmodifiableMap(hooks);
     }
 
-    public boolean isRegistered(Class<? extends Hook<?>> type) {
+    public boolean isRegistered(@NotNull Class<? extends Hook<?>> type) {
         if (this.hooks.containsKey(type)) {
             return true;
         }
@@ -28,22 +30,19 @@ public class HookManager {
         return false;
     }
 
-    public <T extends Hook<?>> T getHook(Class<T> type) {
+    public <T extends Hook<?>> @NotNull T getHook(@NotNull Class<T> type) {
         Hook<?> hook = this.hooks.get(type);
         if (hook != null) {
             return type.cast(hook);
         }
 
-        for (Class<? extends Hook<?>> clazz : hooks.keySet()) {
-            if (type.isAssignableFrom(clazz)) {
-                hook = hooks.get(clazz);
+        for (var clazzMap : hooks.entrySet()) {
+            if (type.isAssignableFrom(clazzMap.getKey())) {
+                return type.cast(clazzMap.getValue());
             }
         }
 
-        if (hook == null) {
-            throw new IllegalArgumentException("No registered hook of type " + type.getName() + "!");
-        }
-        return type.cast(hook);
+        throw new IllegalArgumentException("No registered hook of type " + type.getName() + "!");
     }
 
     /**
@@ -53,7 +52,7 @@ public class HookManager {
         return hooks.values().stream().anyMatch(Hook::active);
     }
 
-    public void registerHook(Hook<?> hook) {
+    public void registerHook(@NotNull Hook<?> hook) {
         hook.onEnable();
         this.hooks.put(hook.getTypeClass(), hook);
     }
