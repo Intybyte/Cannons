28f76154dd25e7c5eed9b663ccca7cd30666d4a7
Intybyte <vaan.testwork@gmail.com>
2025-04-11 10:57:40 +0200

    Make method to time stuff

---
+++ cannons-bukkit/src/main/java/at/pavlov/cannons/utils/TimeUtils.java
@@ -0,0 +1,15 @@
+package at.pavlov.cannons.utils;
+
+import java.util.function.Consumer;
+
+public class TimeUtils {
+    private TimeUtils() {}
+
+    public static void testTime(Runnable tested, Consumer<String> printer, String cause) {
+        long start = System.currentTimeMillis();
+        tested.run();
+        long end = System.currentTimeMillis();
+        long total = start - end;
+        printer.accept("Execution of " + cause + " completed in " + total + " ms");
+    }
+}
