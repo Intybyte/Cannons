ed9ccfa4eb8e0683136a2043494953a5836a1601
Intybyte <vaan.testwork@gmail.com>
2025-05-28 19:13:58 +0200

    Validate more stuff inside Key

--- api-internal/src/main/java/at/pavlov/internal/Key.java
+++ api-internal/src/main/java/at/pavlov/internal/Key.java
@@ -2,7 +2,21 @@ package at.pavlov.internal;
 
 import org.jetbrains.annotations.ApiStatus;
 
+import java.util.Locale;
+import java.util.regex.Pattern;
+
 public record Key(String namespace, String key) {
+    public static final Pattern NAMESPACED_KEY_PATTERN = Pattern.compile("^[a-z0-9._-]+:[a-z0-9._-]+$");
+
+    public Key(String namespace, String key) {
+        this.namespace = namespace.toLowerCase(Locale.ROOT);
+        this.key = key.toLowerCase(Locale.ROOT);
+
+        if (!NAMESPACED_KEY_PATTERN.matcher(this.full()).matches()) {
+            throw new IllegalArgumentException("Invalid NS key: " + this.full());
+        }
+    }
+
     @ApiStatus.Internal
     public static Key cannons(String key) {
         return new Key("cannons", key);
@@ -28,4 +42,12 @@ public record Key(String namespace, String key) {
     public String full() {
         return namespace + ":" + key;
     }
+
+    @Override
+    public String toString() {
+        return "Key[" +
+                "namespace=" + namespace + ", " +
+                "key=" + key + ']';
+    }
+
 }
