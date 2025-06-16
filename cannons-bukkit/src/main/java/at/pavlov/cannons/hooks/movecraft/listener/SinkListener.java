f87856012f329d7ae8cd9f7e995b9fa9ed30eb9f
Intybyte <vaan.testwork@gmail.com>
2025-06-16 20:51:18 +0200

    Add Sinking support (Explode cannons)

---
+++ cannons-bukkit/src/main/java/at/pavlov/cannons/hooks/movecraft/listener/SinkListener.java
@@ -0,0 +1,20 @@
+package at.pavlov.cannons.hooks.movecraft.listener;
+
+import at.pavlov.cannons.Enum.BreakCause;
+import at.pavlov.cannons.cannon.Cannon;
+import at.pavlov.cannons.cannon.CannonManager;
+import at.pavlov.cannons.hooks.movecraft.MovecraftUtils;
+import net.countercraft.movecraft.events.CraftSinkEvent;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+import java.util.Set;
+
+public class SinkListener implements Listener {
+
+    @EventHandler
+    public void onCraftSink(CraftSinkEvent event) {
+        Set<Cannon> cannons = MovecraftUtils.getCannons(event.getCraft());
+        cannons.forEach(cannon -> CannonManager.getInstance().removeCannon(cannon.getUID(), false, true, BreakCause.Explosion));
+    }
+}
