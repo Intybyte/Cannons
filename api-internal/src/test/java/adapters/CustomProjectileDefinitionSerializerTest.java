b3c9d28762a0456b6adeec0daa5f42875af46e13
Intybyte <vaan.testwork@gmail.com>
2025-12-12 19:11:34 +0100

    Correct testing

---
+++ api-internal/src/test/java/adapters/CustomProjectileDefinitionSerializerTest.java
@@ -0,0 +1,11 @@
+package adapters;
+
+import org.junit.jupiter.api.Test;
+
+public class CustomProjectileDefinitionSerializerTest {
+
+    @Test
+    void deserialize() throws Exception {
+        // todo: make testing
+    }
+}
